e2e2950ea4ef9e335d3efc6e3e2a37613dea40a8
Alexander <dev@salex.pro>
2019-03-05 14:25:58 +0200

    login popup

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 /* 
-@codekit-prepend quiet '../../node_modules/jquery/dist/jquery.min.js';
-@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js';
-@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
-@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
+@codekit-prepend quiet '../../node_modules/jquery/dist/jquery.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.motion.min';
+@codekit-prepend quiet '../../node_modules/foundation-sites/dist/js/plugins/foundation.reveal.min';
 */
 
 $(document).foundation();
